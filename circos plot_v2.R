f9dae0e7b6afce87d12719bfe337ffdcb7eab348
lingo1st <whps.happy@gmail.com>
2024-05-18 14:23:02 +0800

    generating circos plot using gbs data

---
+++ circos plot_v2.R
@@ -0,0 +1,74 @@
+library(circlize)
+
+###data資料集 (local recombination rate)
+data <- read.table("C:/Users/lingo1st/Dropbox/林冠瑜/gbs_dataset_result/local_recomb/gbs_local_recomb.txt",header = T)
+data$spline[which(data$spline <=0)] <- 0
+data$map <- gsub("chromosome ","chr",data$map)
+cytoband_df <- data.frame(chrom = data$map,chromStart = data$phys,chromoEnd = data$phys,value1 = data$spline)
+
+#### 重組事件資料集
+
+breakpoint <- read.csv("C:/Users/lingo1st/Dropbox/林冠瑜/gbs_dataset_result/csv file/gbs_breakpoint.csv",header = T)
+y <-  rnorm(n = 6980, mean = 20, sd = 3)
+recomb_df <- data.frame(y = y,x = breakpoint$Pos,chr = breakpoint$Chr)
+recomb_df$chr <- paste0("chr",recomb_df$chr)
+#############建立熱點區間 (hotspot interval)
+local_hotspot <- read.csv("C:/Users/lingo1st/Dropbox/林冠瑜/gbs_dataset_result/local_recomb/local_hotspot_spar_0.1.csv",header = T)
+colnames(local_hotspot)[colnames(local_hotspot) == "map"] <- "chr"
+
+poisson_hotspot <- read.csv("C:/Users/lingo1st/Dropbox/林冠瑜/gbs_dataset_result/csv file/gbs_adjusted_poisson_hotspot.csv",header = T)
+poisson_hotspot$chr <- paste0("chr",poisson_hotspot$chr)
+##centromere region
+cent_min <- c(15.445668,12.625206,17.883934,7.882,11.15,13.201,9.104,11.98,0.993,
+              7.62,11.34,11.06)
+cent_min <- cent_min * 1000000
+cent_max <- c(18.05,15.48,20.51,10.06,13.54,17.84,12.71,14.41,3.93,8.69,13.5,
+              12.2)
+cent_max <- cent_max * 1000000 
+
+################作圖
+circos.clear()
+circos.par("start.degree" = 90,gap.degree = c(1,1,1,1,1,1,1,1,1,1,1,16))
+circos.initializeWithIdeogram(cytoband_df,plotType = c("axis", "labels"),axis.labels.cex = 0.3)
+###local recombination rate
+circos.track(sectors =cytoband_df$chrom , x = cytoband_df$chromStart, y = cytoband_df$value1,
+             panel.fun = function(x, y) {
+               circos.lines(x, y,area = T,border = F,col = "ivory4")
+             })
+###local hotsot
+for (i in 1:nrow(local_hotspot)) {
+  circos.rect(local_hotspot$start[i], -20 ,local_hotspot$end[i], 70,col = "red",lwd = 2,sector.index = local_hotspot$chr[i],border = NA)
+}
+### 加入centromere 區域
+for(i in 1:12){
+  circos.rect(cent_min[i], -20 ,cent_max[i], 70,col = "azure3",lwd = 2,sector.index = paste0("chr",i),border = NA)
+  
+}
+
+###no.2圈: 重組事件分布
+##先創造
+circos.track(sectors = recomb_df$chr , x = recomb_df$x, y = recomb_df$y,ylim = c(0,40),
+             panel.fun = function(x, y) {
+               circos.points(x, y,col = "lightskyblue4",cex = 0.1,pch = 21)
+             })
+
+##poisson hotspot
+for (i in 1:nrow(poisson_hotspot)) {
+  circos.rect(poisson_hotspot$start[i], 10 ,poisson_hotspot$end[i], 30,col = "red",lwd = 3,sector.index = poisson_hotspot$chr[i],border = NA)
+}
+##
+
+circos.genomicDensity(data = den_df,count_by = "number",col = "thistle")
+circos.yaxis(side = "left", sector.index = "chr1",labels.cex = 0.3,track.index = 2,labels.niceFacing = T)
+circos.yaxis(side = "left", sector.index = "chr1",labels.cex = 0.15,track.index = 4,labels.niceFacing = T)
+
+##########加入density plot
+library(vcfR)
+den <- read.vcfR(file = "C:/Users/lingo1st/Dropbox/林冠瑜/final.vcf/final_sample_plus_parent.vcf")
+den_g <- as.data.frame(getFIX(den)) 
+str(den_g)
+den_g$POS <- as.numeric(den_g$POS)
+den_g$CHROM<- as.numeric(den_g$CHROM)
+den_g$CHROM <- paste0("chr",den_g$CHROM)
+den_df <- data.frame(chrom = den_g$CHROM, chromStart = den_g$POS,chromEnd = den_g$POS)
+den_df$chromEnd <- den_df$chromEnd +1
