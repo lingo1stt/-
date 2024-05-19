0cccc16f9fc2004be90c4a82dcb5aeede1183c9f
lingo1st <whps.happy@gmail.com>
2024-05-19 16:33:37 +0800

    old version: input window size and one side length to calculate

--- nuc_diversity.R
+++ nuc_diversity.R
@@ -7,21 +7,47 @@ window_size <- 10000
 fas_data_path <- "C:/Users/lingo1st/Dropbox/林冠瑜/gbs_dataset_result/nuc_test"
 sliding = 0
 one_side_length <- 50000 
+###############data#########################
+overlap_hotspot <- read.csv("C:/Users/lingo1st/Dropbox/林冠瑜/gbs_dataset_result/csv file/gbs_overlap_hotspot.csv",header = T)
+poisson_hotspot <- read.csv("C:/Users/lingo1st/Dropbox/林冠瑜/gbs_dataset_result/csv file//poisson hotspot/gbs_adjusted_poisson_hotspot.csv",header = T)
+local_hotspot <- read.csv("C:/Users/lingo1st/Dropbox/林冠瑜/gbs_dataset_result/csv file/local_recomb_hotspot/gbs_nonoverlap_local_hotspot.csv",header = T)
 
-nuc_diversity <- function(window_size=NA,sliding = 0,fas_data_path = NA, one_side_length ){
+##合併
+hotspot_interval <- rbind(local_hotspot[,c(1,2,3)],poisson_hotspot[,c(2,5,6)],overlap_hotspot[,c(1,2,3)])
+hotspot_interval$type <- rep(c("local","poisson","overlap"),times = c(25,152,21))
+hotspot_interval$chr <- as.numeric(hotspot_interval$chr)
+###加入區間長度
+hotspot_interval$length <- apply(hotspot_interval,1,function(x){
+  as.numeric(x[3]) - as.numeric(x[2])
+})
+##################### function ################################
+###VERSION2: 改成計算整個fasta檔的single nucleotide value，或是分成window size 計算
+nuc_diversity <- function(window_size=NA,sliding = 0,fas_data_path = NA, one_side_length,single_value = F,file_index ){
   cat("############################################","\n","you are calculating nucleotide diversity...###############################","\n")
   # 取得檔案數量
   folder_path <-fas_data_path
   # 列出路徑下的所有檔案
   files <- list.files(folder_path)
+  #將files中的順率調整成跟hotspotinterval 中的一樣(用start位置來分)
+  file_name_start_pos <- unlist(lapply(strsplit(files,split = "_"),function(x){
+    x[1]
+  }))
+  ##file env為hotspot interval dataframe中的名字
+  file_env <- as.character(hotspot_interval$start[which(hotspot_interval$type == "local")])
+  order <- c()
+  for (cor in 1:length(file_env)) {
+    order <- c(order,which(file_env[cor] == file_name_start_pos))
+  }
+  
+  files<- files[order]
   num_files <- length(files)
   if(sliding == 0){
     plot_mat <- matrix(ncol = num_files,nrow = length(seq(1,(2*one_side_length)-sliding,by = window_size))  )
   }else{
     plot_mat <- matrix(ncol = num_files,nrow = length(seq(1,(2*one_side_length)-sliding,by = sliding))  )
   }
-  for (q in 1:num_files) {
-    fas <- read.fasta(paste0(folder_path,"/",files[q]))
+  ###########################################
+    fas <- read.fasta(paste0(folder_path,"/",files[file_index]))
     #前置data及apply使用之function
     name <- names(fas)
     sub <- function(x,pos){
@@ -35,7 +61,7 @@ nuc_diversity <- function(window_size=NA,sliding = 0,fas_data_path = NA, one_sid
     ##計算Pi的迴圈本體
     pi_diversity <- c()
     for (i in seq(1,(2*one_side_length)-window_size+1,length = nrow(plot_mat))) {
-      ##擷取每個區間的長度(1000bp)(temp_list)
+      ##擷取每個區間的長度(temp_list)
       temp_list <- lapply(fas,FUN =  sub,pos = i)
       uni <- unique(temp_list)
       ##比對
@@ -45,7 +71,7 @@ nuc_diversity <- function(window_size=NA,sliding = 0,fas_data_path = NA, one_sid
         freq_list_2[[p]] <- sub_list
       }
       ##將每個個體的名字分配給所屬的不重複序列(freq_list_2) (freq_list為不重複序列本身)
-      for (l in 1:24) {
+      for (l in 1:length(fas)) {
         freq_list <- lapply(uni,FUN = freq,ind = l)
         freq_list_2[[which(freq_list %in% window_size)]] <- append(freq_list_2[[which(freq_list %in% window_size)]],name[l]) 
         
@@ -89,9 +115,9 @@ nuc_diversity <- function(window_size=NA,sliding = 0,fas_data_path = NA, one_sid
       
       
     }
-    plot_mat[1:nrow(plot_mat),q] <- pi_diversity
+    plot_mat[1:nrow(plot_mat),file_index] <- pi_diversity
     
-  }
+  
   
   assign(paste0("plot_mat"),plot_mat,envir = .GlobalEnv)
   write.csv(plot_mat,file = paste0("diverisity value.csv"))
@@ -107,11 +133,11 @@ poisson_recomb_value <- apply(poisson_plot_mat,1,function(x){
   mean(x)
 })
 ####################local recombination################################
-nuc_diversity(window_size = 10000,one_side_length=50000,fas_data_path = "C:/Users/lingo1st/Dropbox/林冠瑜/gbs_dataset_result/local_hotspot_fasta" ,sliding = 0)
-local_plot_mat <- as.data.frame(plot_mat) 
-local_recomb_value <- apply(local_plot_mat,1,function(x){
-  mean(x)
-})
+nuc_diversity(window_size = round(hotspot_interval$length[f]/10,0),
+               one_side_length=round(hotspot_interval$length[f]/2,0),
+               fas_data_path = "C:/Users/lingo1st/Dropbox/林冠瑜/gbs_dataset_result/fasta file/local_hotspot_fasta" ,
+               sliding = 0,
+              file_index = 1)
 local_recomb_value
 locate <- c("-50000","-40000","-30000","-20000","-10000",
             "10000","20000","30000","40000","50000")
@@ -140,4 +166,4 @@ write.csv(plot_df,file = "gbs_nuc_diversity_value.csv")
 write.csv(local_plot_mat,file = "gbs_localhotspot_all_diversity_value.csv")
 write.csv(poisson_plot_mat,file = "gbs_poissonhotspot_all_diversity_value.csv")
 
-p1 + geom_boxplot()
+p1 + geom_boxplot()+
